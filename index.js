1d949562f8b015e8553fa1910d5533fcfa661ed5
Othman AMD <red.fantastic@hotmail.com>
2017-09-25 15:55:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function happyHolidaysTo(name){
   return "Happy holidays, "+name+"!";
 }
 
-function happyHolidayTo(holiday,person){
+function happyHolidayTo(holiday,name){
   return "Happy "+holiday+", "+name+"!";
 }
 
-function holidayCountdown(days,holiday){
+function holidayCountdown(holiday,days){
   return "It\'s "+days+" days until "+holiday+"!";
 }
